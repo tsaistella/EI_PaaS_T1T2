7fda4fd99c8bb3e7632115bb5bda437a6820b449
tsaistella <48236693+tsaistella@users.noreply.github.com>
2019-05-10 13:54:20 +0800

    (1)publisher 使用環境變數設定變數 (2)連線失敗會不斷重複嘗試連線，直到成功為止 (3)間隔10秒才抓取下一筆資料

--- III-ISNR-IB-BRS/publisher/fetchdata.py
+++ III-ISNR-IB-BRS/publisher/fetchdata.py
@@ -11,18 +11,19 @@ def fetchdata():
     
     # === Local ===
     # # RabbitMQ Variables
-    # user = os.getenv('RABBITMQ_USER')
-    # password = os.getenv('RABBITMQ_PASSWORD')
-    # host = os.getenv('RABBITMQ_HOST')
-    # port = os.getenv('RABBITMQ_PORT')
-    # vhost = os.getenv('RABBITMQ_VHOST')
-    
-    # exchange = os.getenv('RABBITMQ_EXCHANGE')
-    user = "isnr"
-    password = "isnr2019"
-    host = "172.20.0.220"
-    port = 5671
-    vhost = "isnr"
+    user = os.getenv('RABBITMQ_USER')
+    password = os.getenv('RABBITMQ_PASSWORD')
+    host = os.getenv('RABBITMQ_HOST')
+    port = os.getenv('RABBITMQ_PORT')
+    vhost = os.getenv('RABBITMQ_VHOST')
+    exchange = os.getenv('RABBITMQ_BPMEXCHANGE')    
+    #user = "isnr"
+    #password = "isnr2019"
+    #host = "172.20.0.220"
+    #port = 5671
+    #vhost = "isnr"
+    #exchange ="bpm"
+
 
     qm = QueueManager(user, password, host, port, vhost, True)
     # TODO: Add feature for switch local and cloud app setting
@@ -39,21 +40,35 @@ def fetchdata():
 
     # rabbitmq_uri = vcap_services['p-rabbitmq'][0]['credentials']['protocols']['amqp']['uri']
 
-    exchange ="bpm"
+  
 
     # initial queues
     init_queue(qm, exchange)
     
     # TODO: Split main function for oop
 
-    queue_manager = "TIABPM"
-    channel = "TAISNR.SVRCONN"
-    host = "10.2.67.76"
-    port = "1414"
-    queue_name = "TAISNR.LOCAL.QUEUE"
-    message = 'Hello from Python!'
-    conn_info = "%s(%s)" % (host, port)
 
+
+    # # IBMMQ Variables
+    queue_manager = os.getenv('IBMQUEUE_MANAGER')    
+    channel = os.getenv("IBMCHANNEL")
+    host = os.getenv("IBMHOST")
+    port = os.getenv("IBMPORT")
+    queue_name = os.getenv("IBMQUEUE_NAME")
+    user = os.getenv('IBMUSER')
+    password = os.getenv('IBMPASSWORD')
+    routing_key = os.getenv('IBMROUTING_KEY')
+    #queue_manager = "TIABPM"
+    #channel = "TAISNR.SVRCONN"
+    #host = "10.2.67.76"
+    #port = "1414"
+    #queue_name = "TAISNR.LOCAL.QUEUE"
+    #user = 'TAISNRBPM'
+    #password = 'Taisnr123456!'
+    #routing_key = 'bpm_rk'
+
+    
+    conn_info = "%s(%s)" % (host, port)
     pymqi.queue_manager = queue_manager
     pymqi.channel = channel
     pymqi.host = host
@@ -63,9 +78,15 @@ def fetchdata():
 
     #qmgr = pymqi.connect(queue_manager, channel, conn_info)
     #Connecting in client mode with username/password credentials
-    user = 'TAISNRBPM'
-    password = 'Taisnr123456!'
-    qmgr = pymqi.connect(queue_manager, channel, conn_info, user, password)
+
+    keep_connect = True
+    while keep_connect:
+        try:           
+            qmgr = pymqi.connect(queue_manager, channel, conn_info, user, password)
+            keep_connect = False
+        except  Exception as pymqierror :
+            print(str(pymqierror))            
+            time.sleep(10)
 
 
     #Connecting in client mode
@@ -73,6 +94,7 @@ def fetchdata():
             
         #How to put the message on a queue
     # try:
+    #     message = 'Hello from Python!'
     #     ibmqueue.put(message)
     # except Exception as identifier:
     #     print(str(identifier))
@@ -83,11 +105,11 @@ def fetchdata():
             #How to get the message off a queue
             msg_body = ibmqueue.get().decode('utf-16')
             print(msg_body)            
-            # routing_key = '{}_rk'.format(api['name'].lower())            
-            routing_key = "bpm_rk"
+            # routing_key = '{}_rk'.format(api['name'].lower())   
             qm.publish(exchange, routing_key, msg_body)
         except  Exception as pymqierror :
             print(str(pymqierror))
+        time.sleep(10)
 
     ibmqueue.close()
 
