4991e4ff3a3948e93c7bcb0ef8e1262c651b14be
tsaistella <48236693+tsaistella@users.noreply.github.com>
2019-05-10 12:30:53 +0800

    新增fetch.sh for 支援Linux環境使用設定環境變數

---
+++ III-ISNR-IB-BRS/subscriber/fetch.sh
@@ -0,0 +1,8 @@
+export RABBITMQ_USER="isnr"
+export RABBITMQ_PASSWORD="isnr2019"
+export RABBITMQ_HOST="172.20.0.220"
+export RABBITMQ_PORT="5671"
+export RABBITMQ_VHOST="isnr"
+export RABBITMQ_BPMEXCHANGE="bpm"
+export RABBITMQ_QUEUENAME="bpm"
+python3 storedata.py
